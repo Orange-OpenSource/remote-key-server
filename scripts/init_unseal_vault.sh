fbe84a2420d493bffe80540232a36a0cfa4a8b01
Feunteun Glenn <glenn.feunteun@orange.com>
2020-03-16 17:19:52 +0100

    Add docker-compose with consul, vault and rks

--- scripts/init_unseal_vault.sh
+++ scripts/init_unseal_vault.sh
@@ -2,8 +2,10 @@
 
 set -e
 
+DOCKER_NETWORK=rks
+
 if [ "$1" != "CI" ]; then
-  DOCKER_CURL_COMMAND="docker run --network=rks curlimages/curl:7.66.0"
+  DOCKER_CURL_COMMAND="docker run --network=$DOCKER_NETWORK curlimages/curl:7.66.0"
 fi
 
 INIT=$($DOCKER_CURL_COMMAND http://rks-vault:8200/v1/sys/init -X PUT -d '{"secret_shares": 1, "secret_threshold": 1}')
